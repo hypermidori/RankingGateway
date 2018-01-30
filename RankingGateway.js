2ee25a9aa4f624589554d5eeb700cfe9814b4046
hypermidori <hypermidori@gmail.com>
2018-01-30 13:35:31 +0900

    accHubに接続。topic対応。

--- RankingGateway.js
+++ RankingGateway.js
@@ -1,6 +1,8 @@
 
+'use strict'
+const http = require('http');
 const fs = require("fs");
-const server = require("http").createServer(function (req, res) {
+const server = http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     var output = fs.readFileSync("./index.html", "utf-8");
     res.end(output);
@@ -8,42 +10,80 @@ const server = require("http").createServer(function (req, res) {
 const io = require("socket.io").listen(server);
 
 let users = {};     // 接続中ユーザ
+let topics = {};    // 現在購読されているトピック
 
 // ユーザイベント用リスナ
 io.sockets.on("connection", (socket) => {
 
-    // ユーザが接続
-    socket.on("connected", (userData) => {
+    // ユーザ情報更新
+    socket.on("user_data_update", (userData) => {
+        console.log("user_data_update");
+        console.log(userData);
+
         users[socket.id] = userData;
+
+        // topicに購読ユーザ追加
+        const topic = userData.topic;
+        if (topic) {
+            if (!topics[topic]) {
+                topics[topic] = {};
+            }
+
+            topics[topic][socket.id] = userData;
+        }
     });
 
     // 接続終了
     socket.on("disconnect", (socket) => {
         if (users[socket.id]) {
+            userData = users[socket.id];
+
+            // 購読中トピックから接続終了したユーザ情報を削除
+            if (topics[userData.topic]) {
+                if (topics[topic][socket.id]) {
+                    delete topics[topic][socket.id];
+                }
+            }
+
+            // 接続中ユーザからユーザ削除
             delete users[socket.id];
         }
     });
 });
 
-// テスト用
-const rankingInfo = [
-    {
-        rank: 1,
-        name: "Kinoko"
-    },
-    {
-        rank: 2,
-        name: "Takenoko"
-    },
-    {
-        rank: 3,
-        name: "Kikori"
-    }
-]
+
+// トピック毎のユーザにaccデータをプッシュ
+const pushAcc = (topicName) => {
+    let url = "http://acchub.net:8080/" + topicName;
+    console.log("getAccData: " + url);
+
+    // トピックのaccデータを取得
+    http.get(url, (res) => {
+        let body = '';
+        res.setEncoding('utf8');
+
+        res.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        res.on('end', (res) => {
+            let accData = JSON.parse(body);
+            io.sockets.emit("ranking_update", accData);
+
+            // topic購読ユーザーのみにpush
+            Object.keys(topics[topicName]).forEach((socketId)=> {
+                io.to(socketId).emit("ranking_update", accData);
+            });
+        });
+    }).on('error', (e) => {
+        console.log(e.message); //エラー時
+    });
+}
 
 // 順位サーバーへのポーリング、ユーザへの通知
-// TODO 現状は固定の値(rankingInfo)
 setInterval(() => {
-    io.sockets.emit("ranking_update", rankingInfo);
+    Object.keys(topics).forEach((topicName) => {
+        pushAcc(topicName);
+    })
 }, 1000);
 
--- TestClient.js
+++ TestClient.js
@@ -1,7 +1,23 @@
-const socket = require('socket.io-client')('http://localhost:37564');
+const io = require('socket.io-client')
 
+const socket = io(
+    'http://localhost:37564',
+    {autoConnect: false}
+);
 socket.on("ranking_update",(rankingInfo)=>{
     console.log("incoming ranking_update:");
     console.log(rankingInfo);
     console.log("\n");
 });
+
+socket.on("connect", ()=>{
+    console.log("connect");
+    const userData = {
+        "name":"testUser",
+        "topic":"testTopic"
+    }
+    socket.emit("user_data_update", userData);
+});
+
+socket.open();
+
